0ebf3a04c703b4389f41a32c0727347b6367c9eb
ErnestoCDesktop <ernestocabanap@gmail.com>
2020-04-26 22:20:35 -0500

    Added image file to recipe model

--- app/app/settings.py
+++ app/app/settings.py
@@ -136,7 +136,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
 
+MEDIA_ROOT = '/vol/web/media'
+STATIC_ROOT = '/vol/web/static'
+
+# 127.0.0.1:8000/media/
 
 # Custom User Model
 AUTH_USER_MODEL = 'core.User'
